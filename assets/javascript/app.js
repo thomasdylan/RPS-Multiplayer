05772622929522857913bb0b9b440078a7e0ca45
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-25 17:48:50 -0600

    init

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//Psuedo needs
+/*
+    User1 & User2
+    selection for each player
+    Rock, Paper, Scissor buttons
+    comparison
+    has player submitted answer? If so they can't enter another until game is reset.
+    UserX beat UserX
+    User score
+    reset game
+    Chat box in firebase [array or most recent message]
+*/
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDYc0_MeU4ie7bUtWVwWbue0LFWqATiL34",
+    authDomain: "rps-multiplayer-651bf.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-651bf.firebaseio.com",
+    projectId: "rps-multiplayer-651bf",
+    storageBucket: "",
+    messagingSenderId: "837989033759",
+    appId: "1:837989033759:web:44382551724f501397f22a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
